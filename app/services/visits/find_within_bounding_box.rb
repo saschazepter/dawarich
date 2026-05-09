3ba1e94d8616fd48970f83756f7efe7b50e77d39
Eugene Burmakin <iamfrey@gmail.com>
2026-05-09 22:00:41 +0200

    Lift envelope SQL constants out of method body, eager-load through joins

--- app/services/visits/find_within_bounding_box.rb
+++ app/services/visits/find_within_bounding_box.rb
@@ -3,14 +3,6 @@
 module Visits
   # Finds visits in a selected area on the map
   class FindWithinBoundingBox
-    def initialize(user, params)
-      @user = user
-      @sw_lat = params[:sw_lat].to_f
-      @sw_lng = params[:sw_lng].to_f
-      @ne_lat = params[:ne_lat].to_f
-      @ne_lng = params[:ne_lng].to_f
-    end
-
     PLACE_INSIDE = 'places.lonlat IS NOT NULL AND ' \
                    'ST_Contains(ST_MakeEnvelope(?, ?, ?, ?, 4326), ' \
                    'ST_SetSRID(places.lonlat::geometry, 4326))'
@@ -19,10 +11,19 @@ module Visits
                   'ST_Contains(ST_MakeEnvelope(?, ?, ?, ?, 4326), ' \
                   'ST_SetSRID(ST_MakePoint(areas.longitude, areas.latitude), 4326))'
 
+    def initialize(user, params)
+      @user = user
+      @sw_lat = params[:sw_lat].to_f
+      @sw_lng = params[:sw_lng].to_f
+      @ne_lat = params[:ne_lat].to_f
+      @ne_lng = params[:ne_lng].to_f
+    end
+
     def call
       user.scoped_visits
-          .includes(:place, :area)
           .left_outer_joins(:place, :area)
+          .includes(:place, :area)
+          .references(:place, :area)
           .where(
             "(#{PLACE_INSIDE}) OR (#{AREA_INSIDE})",
             sw_lng, sw_lat, ne_lng, ne_lat,
